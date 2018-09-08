951ec583cc8a80583e35480d12ba84db8b4f44fd
Brent Vaalburg <btvaalburg@gmail.com>
2018-09-08 12:17:04 -0600

    Update sts9_setlist_url_gen.py

--- sts9_setlist_url_gen.py
+++ sts9_setlist_url_gen.py
@@ -1,26 +1,7 @@
 from bs4 import BeautifulSoup
 import re
 
-in_html = ['HTML/URL/Music _ STS9 _ Live _ By Year _ 2017.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2016.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2015.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2014.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2013.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2012.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2011.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2010.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2009.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2008.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2007.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2006.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2005.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2004.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2003.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2002.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2001.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 2000.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 1999.html',
-           'HTML/URL/Music _ STS9 _ Live _ By Year _ 1998.html']
+in_html = ['HTML/URL/Music _ STS9 _ Live _ By Year _ 2017.html',]
 
 txt_file = open('urls.txt', 'a')
 
